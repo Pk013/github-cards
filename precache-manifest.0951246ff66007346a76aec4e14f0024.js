991e24189dea23896e83b5965ded94b3ba280efe
morthos <pathshan61@gmail.com>
2018-11-28 14:00:56 -0500

    Updates

---
+++ precache-manifest.0951246ff66007346a76aec4e14f0024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17ffec8febb6aa5c8537",
+    "url": "/github-cards/./static/js/runtime~main.17ffec8f.js"
+  },
+  {
+    "revision": "f5043095576b307f7324",
+    "url": "/github-cards/./static/js/main.f5043095.chunk.js"
+  },
+  {
+    "revision": "d5420a33caa6b496d129",
+    "url": "/github-cards/./static/js/1.d5420a33.chunk.js"
+  },
+  {
+    "revision": "f5043095576b307f7324",
+    "url": "/github-cards/./static/css/main.85fd3d8e.chunk.css"
+  },
+  {
+    "revision": "6c1a0199a3e7f9424949259474335611",
+    "url": "/github-cards/./index.html"
+  }
+];
